c62c85270a5452b4ba1ad5bd5b119a0cdccbe94c
Lambert-01 <nlambert833@gmail.com>
2026-06-28 11:13:34 +0200

    Add Gunicorn configuration file for server settings

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+import multiprocessing
+import os
+
+
+bind = f"0.0.0.0:{os.environ.get('PORT', '5000')}"
+workers = int(os.environ.get("WEB_CONCURRENCY", max(2, multiprocessing.cpu_count())))
+threads = int(os.environ.get("GUNICORN_THREADS", "2"))
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
+graceful_timeout = int(os.environ.get("GUNICORN_GRACEFUL_TIMEOUT", "30"))
+keepalive = int(os.environ.get("GUNICORN_KEEPALIVE", "5"))
+accesslog = "-"
+errorlog = "-"
+loglevel = os.environ.get("GUNICORN_LOG_LEVEL", "info")
